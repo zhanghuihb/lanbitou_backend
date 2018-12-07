7d2d0fb8909e8653271555c2018819aa7ed858f9
zhanghuihb <zhanghuihb@126.com>
2018-12-07 14:30:55 +0800

    first commit

---
+++ lanbitou/src/main/java/com/burton/lanbitou/respository/OpinionRepository.java
@@ -0,0 +1,12 @@
+package com.burton.lanbitou.respository;
+
+import com.burton.common.domain.Opinion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Tainy
+ * @date 2018/6/27 16:14
+ */
+public interface OpinionRepository extends JpaRepository<Opinion, Integer>{
+
+}
---
+++ lts/src/main/java/com/burton/lts/dubbo/LtsDubboServiceImpl.java
@@ -0,0 +1,13 @@
+package com.burton.lts.dubbo;
+
+import com.burton.common.dubbo.lts.LtsDubboService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Tainy
+ * @date 2018/6/21 19:50
+ */
+@Service
+public class LtsDubboServiceImpl implements LtsDubboService{
+
+}
